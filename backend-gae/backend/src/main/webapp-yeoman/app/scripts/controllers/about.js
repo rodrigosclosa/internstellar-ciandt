0612e978a557a3abae0e9394088ef7e21f683e55
Rodrigo Sclosa <rodrigogs@ciandt.com>
2016-10-07 20:30:37 -0300

    Criação do projeto do dashboard com o yeoman.

---
+++ backend-gae/backend/src/main/webapp-yeoman/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internstellarDashApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the internstellarDashApp
+ */
+angular.module('internstellarDashApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ backend-gae/backend/src/main/webapp-yeoman/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internstellarDashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the internstellarDashApp
+ */
+angular.module('internstellarDashApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
